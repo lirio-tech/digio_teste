459ce0f41a211cfa3838695fa0e4a4ea392218d4
Diego Lirio <diegolirio.dl@gmail.com>
2020-09-13 22:09:51 -0300

    Deploy branch and pipe devops

--- devops/develop/update-version.sh
+++ devops/develop/update-version.sh
@@ -2,4 +2,4 @@ OLD_VERSION=$(cat pom.xml | grep "^    <version>.*</version>$" | awk -F'[><]' '{
 #MVN_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 mvn release:update-versions          
 NEW_VERSION=$(cat pom.xml | grep "^    <version>.*</version>$" | awk -F'[><]' '{print $3}')
-sed -i "s/$OLD_VERSION/$NEW_VERSION/g" devops/digio-kubernetes.yml 
+sed -i "s/$OLD_VERSION/$NEW_VERSION/g" devops/develop/digio-kubernetes.yml 
