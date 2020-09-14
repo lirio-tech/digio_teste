4f7deeb93b43a93acf987a61ba8812f4f214471b
Diego Lirio <diegolirio.dl@gmail.com>
2020-09-13 23:41:26 -0300

    SNAPSHOT DEVELOP

--- devops/develop/update-version.sh
+++ devops/develop/update-version.sh
@@ -1,3 +1,11 @@
+SNAPSHOT="SNAPSHOT"
+NEW_VERSION=$(cat pom.xml | grep "^    <version>.*</version>$" | awk -F'[><]' '{print $3}')
+
+case "$NEW_VERSION" in
+    *$SNAPSHOT* ) echo "Version OK";;
+    * ) echo "Error... version should to be $SNAPSHOT";;
+esac
+
 OLD_VERSION=$(cat pom.xml | grep "^    <version>.*</version>$" | awk -F'[><]' '{print $3}')
 #MVN_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 mvn release:update-versions          
