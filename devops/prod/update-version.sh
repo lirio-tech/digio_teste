ad00dc8e1e619b0e2f1a4227efcdc3e3a84f679f
Diego Lirio <diegolirio.dl@gmail.com>
2020-09-13 23:23:16 -0300

    Config prod

--- devops/prod/update-version.sh
+++ devops/prod/update-version.sh
@@ -1,5 +1,3 @@
-
-
 SNAPSHOT="SNAPSHOT"
 NEW_VERSION=$(cat pom.xml | grep "^    <version>.*</version>$" | awk -F'[><]' '{print $3}')
 
@@ -10,5 +8,5 @@ esac
 
 NEW_VERSION_RELEASE=$(echo $NEW_VERSION | sed -e "s/$SNAPSHOT/RELEASE/g")
 
-mvn --batch-mode release:update-versions -DdevelopmentVersion=$NEW_VERSION_RELEASE
+mvn versions:set -DnewVersion=$NEW_VERSION_RELEASE
 sed -i "s/REPLACE_VERSION/$NEW_VERSION_RELEASE/g" devops/prod/digio-kubernetes-deployment-v2.yml 
